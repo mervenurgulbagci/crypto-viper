cf89600e49c7bd46b65bc6096860e62b3b4f2476
mervenurgulbagci <mervenurgulbagci@gmail.com>
2023-02-23 16:30:47 +0300

    add main codes

--- CryptoViper/Presenter.swift
+++ CryptoViper/Presenter.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFailed
+}
+
+protocol AnyPresenter {
+    var router : AnyRouter? {get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto(result : Result<[Crypto],Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var view: AnyView?
+    var router: AnyRouter?
+    var interactor: AnyInteractor?{
+        didSet{
+            interactor?.downloadCryptos()
+        }
+    }
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let cryptos):
+            view?.update(with: cryptos)
+        case .failure(_):
+            view?.update(with: "Error!")
+        }
+    }
+}
